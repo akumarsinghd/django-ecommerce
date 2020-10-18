235f0297b6a73ece5767079a49719a7aaa6bf6ce
akumarsinghd <amansingh1032000@gmail.com>
2020-10-18 23:32:20 +0530

    Security fixes

--- mac/settings.py
+++ mac/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b7$%whwlht3-d#$8wa581f**cp$3o5ic+7d7=wlhuu_$4tb-7n'
+SECRET_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
